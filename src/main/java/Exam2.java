d6d81b5c1b67df6de4b996efdc8b9e7345aac573
chris <cpierrecharles21@students.desu.edu>
2022-12-05 14:19:54 -0500

    Completed

--- src/main/java/Exam2.java
+++ src/main/java/Exam2.java
@@ -15,5 +15,17 @@ public class Exam2
     public static void bubble(int[] anArray)
     {
         // TODO: replace this line with your code
+        for(int index=0; index<anArray.length-1; index++){
+            int num1 = 0;
+            int num2 = 0;
+            if(anArray[index] > anArray[index+1]){
+                num1 = anArray[index];
+                num2 = anArray[index+1];
+                anArray[index+1] = num1;
+                anArray[index] = num2;
+            }else{
+
+            }
+        }
     }
 }
